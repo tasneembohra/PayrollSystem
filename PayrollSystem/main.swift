f6cd55f3309200911ec3efd3ce18c4061b8f3984
Tasneem Bohra <tasneem.bohra@hotmail.com>
2018-09-10 18:38:23 -0400

    Initial Commit

---
+++ PayrollSystem/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  PayrollSystem
+//
+//  Created by Tasneem on 10/09/18.
+//  Copyright © 2018 Lambton College. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
