09464f4f4cffe7127cb39eaac9d08a1311625d4b
Tasneem <tasneem@Tasneems-MacBook-Pro.local>
2018-09-10 20:28:49 -0400

    read from JSON

--- PayrollSystem/main.swift
+++ PayrollSystem/main.swift
@@ -8,5 +8,25 @@
 
 import Foundation
 
-print("Hello, World!")
+func readJSON() {
+    if let path  = Bundle.main.path(forResource: "employees", ofType: "json") {
+        do {
+            print(path)
+            let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+            print(data)
+            let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves)
+            if let emplyees = jsonResult as? [Any] {
+                for employee in emplyees {
+                    if let dict = employee as? Dictionary<String, Any> {
+                        print(dict["name"])
+                    }
+                }
+            }
+        } catch {
+            print("Error while reading")
+        }
+    }
+}
+
+readJSON()
 
--- PayrollSystem/utilities/JSONUtility.swift
+++ PayrollSystem/utilities/JSONUtility.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+
+func readEmplyeeData(fromFile:String) {
+    if let path  = Bundle.main.path(forResource: fromFile, ofType: "json") {
+        do {
+            print(path)
+            let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+            print(data)
+            let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves)
+            if let emplyees = jsonResult as? [Any] {
+                for employee in emplyees {
+                    if let dict = employee as? Dictionary<String, Any> {
+                        print(dict["name"])
+                    }
+                }
+            }
+        } catch {
+            print("Error while reading")
+        }
+    }
+}
