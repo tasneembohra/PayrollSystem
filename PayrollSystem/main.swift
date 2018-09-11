c1817c42542015dda0527f4e836d938263fc3690
Tasneem <tasneem@Tasneems-MacBook-Pro.local>
2018-09-10 21:11:05 -0400

    Read function from utility

--- PayrollSystem/main.swift
+++ PayrollSystem/main.swift
@@ -8,25 +8,4 @@
 
 import Foundation
 
-func readJSON() {
-    if let path  = Bundle.main.path(forResource: "employees", ofType: "json") {
-        do {
-            print(path)
-            let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
-            print(data)
-            let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves)
-            if let emplyees = jsonResult as? [Any] {
-                for employee in emplyees {
-                    if let dict = employee as? Dictionary<String, Any> {
-                        print(dict["name"])
-                    }
-                }
-            }
-        } catch {
-            print("Error while reading")
-        }
-    }
-}
-
-readJSON()
-
+JSONUtility.readEmplyeeData(fromFile: "employees")
--- PayrollSystem/utilities/JSONUtility.swift
+++ PayrollSystem/utilities/JSONUtility.swift
@@ -7,23 +7,22 @@
 //
 
 import Foundation
-
-func readEmplyeeData(fromFile:String) {
-    if let path  = Bundle.main.path(forResource: fromFile, ofType: "json") {
-        do {
-            print(path)
-            let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
-            print(data)
-            let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves)
-            if let emplyees = jsonResult as? [Any] {
-                for employee in emplyees {
-                    if let dict = employee as? Dictionary<String, Any> {
-                        print(dict["name"])
+class JSONUtility {
+    static func readEmplyeeData(fromFile:String) {
+        if let path  = Bundle.main.path(forResource: fromFile, ofType: "json") {
+            do {
+                let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+                let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves)
+                if let emplyees = jsonResult as? [Any] {
+                    for employee in emplyees {
+                        if let dict = employee as? Dictionary<String, Any> {
+                            print(dict["name"] as? String)
+                        }
                     }
                 }
+            } catch {
+                print("Error while reading")
             }
-        } catch {
-            print("Error while reading")
         }
     }
 }
