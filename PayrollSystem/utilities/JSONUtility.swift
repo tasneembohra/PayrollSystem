83349bfc34105ac119304ace16ef99b7f475252e
Tasneem <tasneem@Tasneems-MacBook-Pro.local>
2018-09-11 20:26:50 -0400

    Created fixed based employee

--- PayrollSystem/utilities/JSONUtility.swift
+++ PayrollSystem/utilities/JSONUtility.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 class JSONUtility {
-    static func readEmplyeeData(fromFile:String) { // Return employee object
+    static func readEmplyeeData(fromFile:String) -> Employee? { // Return employee object
         if let path  = Bundle.main.path(forResource: fromFile, ofType: "json") {
             do {
                 let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
@@ -16,18 +16,28 @@ class JSONUtility {
                 if let emplyees = jsonResult as? [Any] {
                     for employee in emplyees {
                         if let dict = employee as? Dictionary<String, Any> {
-                            // Initialize employee object
+                            var employee: Employee
                             let name = dict["name"] as? String ?? "NA"
-                            print(name)
+                            let age = dict["age"] as? Int
                             let employement = dict["employement"] as? Dictionary<String, Any>
-                            if (employement != nil) {
-                                // Initialize emplyoment object
-                                switch (employement?["type"] as! String) {
-                                    case "parttime": print("parttime")
-                                    case "fulltime": print("fulltime")
-                                    case "intern": print("intern")
+                            if (employement == nil) { return nil }
+                            // Initialize emplyoment object
+                            switch (employement?["type"] as! String) {
+                            case "parttime":
+                                let subType = employement!["sub_type"] as! String
+                                if (subType == "fixed") {
+                                    let rate = employement!["rate"] as! Double
+                                    let hours = employement!["hours_worked"] as! Double
+                                    let fixedAmount = employement!["fixed_amount"] as! Int
+                                    employee = FixedBasedPartTime(name: name, age: age, rate: rate, hoursWorked: hours, fixedAmmount: fixedAmount)
+                                } else if (subType == "commission") {
+                                        
+                                    } else {
+                                        return nil
+                                    }
+                                case "fulltime": print("fulltime")
+                                case "intern": print("intern")
                                 default: print("")
-                                }
                             }
                             let vehicle = dict["vehicle"] as? Dictionary<String, Any>
                             if (vehicle != nil) {
