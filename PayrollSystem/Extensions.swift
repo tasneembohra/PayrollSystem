f5ada886e5f1e54fe9bcf4c43f1c90ee57d9c135
Daian <daian.aiziatov@gmail.com>
2018-09-18 00:05:24 -0400

    extensions

---
+++ PayrollSystem/Extensions.swift
@@ -0,0 +1,25 @@
+//
+//  Extensions.swift
+//  PayrollSystem
+//
+//  Created by Daian Aiziatov on 17/09/2018.
+//  Copyright © 2018 Lambton College. All rights reserved.
+//
+
+import Foundation
+
+extension Int {
+    var CanadianDollars: String
+    {
+        return "CAD \(self)"
+    }
+}
+
+extension Double
+{
+    var CanadianDollars: String
+    {
+        return "CAD \(self)"
+    }
+    
+}
