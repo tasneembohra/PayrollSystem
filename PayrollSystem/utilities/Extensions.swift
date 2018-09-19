32540457bf5da28a207c5fcb9cb27dbf90fd43cd
Daian <daian.aiziatov@gmail.com>
2018-09-18 20:38:00 -0400

    adding extensions

--- PayrollSystem/utilities/Extensions.swift
+++ PayrollSystem/utilities/Extensions.swift
@@ -8,16 +8,30 @@
 
 import Foundation
 
-extension Int {
-    var CAD: String
-    {
-        return "CAD \(self)"
-    }
-}
-
 extension Double {
-    var CAD: String
+    
+    func Formatting() -> String
     {
-        return "CAD \(self)"
+        let format = NumberFormatter()
+        format.currencyCode = "CAD"
+        format.numberStyle = .currency
+        if let formatValue = format.string(from: self as NSNumber) {
+            return "\(formatValue)"
+        }
+        return "\(self)"
+    }
+    
+    func hours() -> String
+    {
+        let format = NumberFormatter()
+        format.numberStyle = .decimal
+        if let formatValue = format.string(from: self as NSNumber) {
+            if self > 1{
+                return "\(formatValue) hours"
+            } else {
+                return "\(formatValue) hour"
+            }
+        }
+        return "\(self)"
     }
 }
